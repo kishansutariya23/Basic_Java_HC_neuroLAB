713b31762f56b44bc9364d006ab7779dd64c44a4
kishansutariya23 <kishansutariya7533@gmail.com>
2023-03-17 14:00:39 +0530

    university program

---
+++ Programming with Java/Compare2No.java
@@ -0,0 +1,21 @@
+public class Compare2No {
+    public static void main(String args[]) {
+
+        //declare 2 variables
+
+        int numberOne = 356;
+        int numberTwo = 356;
+
+
+        if (numberOne > numberTwo) {
+            System.out.println(numberOne + " is greater than " + numberTwo);
+        } else if (numberOne < numberTwo) {
+            System.out.println(numberOne + " is less than " + numberTwo);
+        } else {
+            System.out.println(numberOne + " is equal to " + numberTwo);
+        }
+
+
+
+    }
+}
---
+++ Programming with Java/FindOddEven.java
@@ -0,0 +1,19 @@
+// package Programming with Java;
+
+public class FindOddEven {
+    public static void main(String args[]) {
+
+        int upperLimit = 200;
+
+        for (int i = 1; i <= 200; i++){
+            //System.out.println("Number is " + i);
+
+            if (i % 2 == 0){
+                System.out.println("Number is " + i);
+            }
+
+
+        }
+
+    }
+}
