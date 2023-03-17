8dc9ef55699b9b26baefb0bf3ddc10f9b20e8a35
kishansutariya23 <kishansutariya7533@gmail.com>
2023-03-17 13:27:16 +0530

    conditional done

---
+++ Conditional Statements/Relation.java
@@ -0,0 +1,19 @@
+// package Conditional Statements;
+
+public class Relation {
+     public static void main(String args[]){
+
+int number1 = 10;
+        int number2 = 15;
+
+
+        System.out.println(number1 == number2); //false
+        System.out.println(number1 != number2); //true
+        System.out.println(number1 > number2); //false
+        System.out.println(number1 < number2); //true
+        System.out.println(number1 <= number2); //true
+        System.out.println(number1 >= number2); //false
+
+    }
+}
+
---
+++ Conditional Statements/arithmetic.java
@@ -0,0 +1,23 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        System.out.printf("arithmetic");
+        System.out.println();
+        
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 35;
+
+
+        System.out.println(number3 % number1);
+
+//        number1 = number1 + 1;
+
+//        number1++;
+
+        System.out.println(number1++);
+        System.out.println(number1);
+
+        System.out.println(++number1);
+        
+    }
+}
